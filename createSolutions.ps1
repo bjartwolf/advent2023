e14f98d9e8bb54117c59897b11d1b71d0e8ee544
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-11-30 19:54:40 +0100

    Init all days

---
+++ createSolutions.ps1
@@ -0,0 +1,11 @@
+$rootDirectory = "C:\code\advent2023"
+
+1..24 | ForEach-Object {
+    $dayFolderName = "day_$_"
+    New-Item -Path $rootDirectory -Name "${dayFolderName}" -ItemType Directory
+    $srcFolderPath = Join-Path -Path $rootDirectory -ChildPath $dayFolderName
+    cd $srcFolderPath 
+    dotnet new console -lang F#
+    New-Item -Path $srcFolderPath -Name "input1.txt" -ItemType File
+    New-Item -Path $srcFolderPath -Name "input2.txt" -ItemType File
+} 
