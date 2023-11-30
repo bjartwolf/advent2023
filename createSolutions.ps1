4f5cdea638bf11b0cb507117ee8aa113e35d2500
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-11-30 20:27:06 +0100

    .net8 and a solution file for nrunch

--- createSolutions.ps1
+++ createSolutions.ps1
@@ -6,6 +6,10 @@ $rootDirectory = "C:\code\advent2023"
     $srcFolderPath = Join-Path -Path $rootDirectory -ChildPath $dayFolderName
     cd $srcFolderPath 
     dotnet new xunit -lang F#
+    dotnet new sln    
+    dotnet sln add $dayFolderName".fsproj"
+    del Tests.fs
+    copy ..\Program.fs .
     New-Item -Path $srcFolderPath -Name "input1.txt" -ItemType File
     New-Item -Path $srcFolderPath -Name "input2.txt" -ItemType File
 } 
