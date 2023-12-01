da6674e7095553d6ecdbc3cc0abc672b5707418f
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-01 13:05:25 +0100

    That actually worked

--- day_1/Program.fs
+++ day_1/Program.fs
@@ -9,43 +9,87 @@ module Input =
     let textNumbers = numbers |> List.map fst
     let digits = numbers |> List.map snd 
 
-    let findTxtNumberBy (comparer : int list -> int) (line: string): (int * string) option =
+    let indexOfNumber (inputString: string) (substring: string) =
+        let rec findIndexes (startIndex: int) (acc: int list) =
+            match inputString.IndexOf(substring, startIndex) with
+            | -1 -> List.rev acc
+            | index -> findIndexes (index + substring.Length) (index::acc)
+        findIndexes 0 []
+
+    [<Fact>]
+    let indexestest() = 
+        let line = "eightsevenfooneeight"
+        let nr = "eight"
+        let indexes = indexOfNumber line nr 
+        Assert.True([0;15] = indexes)
+
+    let firstTxtNumber (line: string): (int * int) option =
         try 
-            let positions = textNumbers |> List.map (fun nr -> line.IndexOf(nr)) 
-            let positionsFound = positions |> List.where (fun x -> x >=0)
-            let minPositionFound = positionsFound |> comparer 
-            let indexOfMin = List.findIndex (fun x -> x = minPositionFound) positions
-            Some(minPositionFound, textNumbers[indexOfMin])
-        with _ ->
+            let ofoo = textNumbers |> List.mapi (fun i number -> (i, indexOfNumber line number))  |> List.map (fun (x,y) -> if y.IsEmpty then (x,None ) else (x, Some (List.min y)))
+            let (y,i)= ofoo |> List.minBy (fun (_, y) -> match y with | Some x -> x | None -> 999)
+            match i with 
+                | Some i -> Some(i, y)
+                | None -> None
+        with ex ->
             None
+    [<Fact>]
+    let testsss1 () = 
+        let input = "two1nine" 
+        Assert.Equal(Some(0,2), firstTxtNumber input) 
 
-    let firstTxtNumber (line: string): (int * string) option =
-        findTxtNumberBy List.min line 
-
-    let lastTxtNumber(line: string): (int * string) option =
-        findTxtNumberBy List.max line 
+    let lastTxtNumber(line: string): (int * int) option =
+        try 
+            let ofoo = textNumbers |> List.mapi (fun i number -> (i, indexOfNumber line number))  |> List.map (fun (x,y) -> if y.IsEmpty then (x,None ) else (x, Some (List.max y)))
+            let (y,i)= ofoo |> List.maxBy (fun (_, y) -> match y with | Some x -> x | None -> -999)
+            match i with 
+                | Some i -> Some(i, y)
+                | None -> None
+        with ex ->
+            None
 
     [<Fact>]
     let firstText() = 
         let input = "7pqrstsixteen" 
-        Assert.Equal(Some (6,"six"), firstTxtNumber input) 
+        Assert.Equal(Some (6,6), firstTxtNumber input) 
+
+    [<Fact>]
+    let LastText() = 
+        let input = "5hrlslhpnine4seven4bzjtteightveighttv4" 
+        Assert.Equal(Some (30,8), lastTxtNumber input) 
+    
+    let isNumber (char: char) : bool =
+        digits |> List.contains (char.ToString())
+
+    [<Fact>]
+    let IsNrTest() = 
+        let input = char '7' 
+        Assert.Equal(true, isNumber input) 
+
+    [<Fact>]
+    let IsNrTest2() = 
+        let input = char 's' 
+        Assert.Equal(false, isNumber input) 
 
     let findDigitNumberBy (comparer: int list -> int) (line: string): (int * string) option =
         try 
-            let positions = digits |> List.map (fun nr -> line.IndexOf(nr)) 
-            let positionsFound = positions |> List.where (fun x -> x >=0)
-            let minPositionFound = positionsFound |> comparer 
-            let indexOfMin = List.findIndex (fun x -> x = minPositionFound) positions
-            Some (minPositionFound, digits[indexOfMin])
+            let txtThatAreNumbers = line |> Seq.toList |> List.mapi (fun i x -> ((isNumber x), i)) |> List.filter (fun (isNum, _) -> isNum) |> List.map (fun (_,i) -> i)
+            let position = txtThatAreNumbers |> comparer
+            let lastDigit = line |> Seq.toList |> List.item position
+            Some (position, lastDigit.ToString())
         with _ ->
             None
-
 
     let firstDigitNumber (line: string): (int * string) option =
         findDigitNumberBy List.min line
 
     let lastDigitNumber (line: string): (int * string) option =
         findDigitNumberBy List.max line
+
+
+    [<Fact>]
+    let LastDigitTest() = 
+        let input = "5hrlslhpnine4seven4bzjttvtv4" 
+        Assert.Equal(Some ((input.Length - 1),"4"), lastDigitNumber input) 
 
     [<Fact>]
     let firstDigitNumberTest() = 
@@ -64,22 +108,23 @@ module Input =
 
     let firstAndLast (inputline: string): int=
         let firstDigit = firstDigitNumber inputline
-        let lastDigit = lastDigitNumber inputline
         let firstTxt = firstTxtNumber inputline
+
+        let lastDigit = lastDigitNumber inputline
         let lastTxt = lastTxtNumber inputline
 
         let firstNumber = match (firstDigit, firstTxt) with
                                     | Some (i,x), Some (j,_) when i < j -> x 
-                                    | Some (i,_), Some (j,y) when i > j -> numbersMap.[y] 
-                                    | None, Some (j,y) -> numbersMap.[y] 
-                                    | Some(i,x), None -> x 
+                                    | Some (i,_), Some (j,y) when i > j -> y.ToString() 
+                                    | None, Some (_,y) -> y.ToString()
+                                    | Some(_,x), None -> x.ToString()
                                     | _ -> failwith "oopsy"
 
         let lastNumber = match (lastDigit, lastTxt) with
-                                    | Some (i,x), Some (j,y) when i > j -> x 
-                                    | Some (i,x), Some (j,y) when i < j -> numbersMap.[y] 
-                                    | None, Some (j,y) -> numbersMap.[y] 
-                                    | Some(i,x), None -> x 
+                                    | Some (i,x), Some (j,_) when i > j -> x 
+                                    | Some (i,_), Some (j,y) when i < j -> y.ToString()
+                                    | None, Some (_,y) -> y.ToString() 
+                                    | Some(_,x), None -> x 
                                     | _ -> failwith "oopsy"
 
         int(firstNumber + lastNumber)
@@ -120,9 +165,16 @@ module Input =
         Assert.Equal(14, firstAndLast input) 
 
     [<Fact>]
+    let testi7x () = 
+        let input = "5hrlslhpnine4seven4bzjttvtv4" 
+        Assert.Equal(54, firstAndLast input) 
+
+    [<Fact>]
     let testi7 () = 
         let input = "7pqrstsixteen" 
         Assert.Equal(76, firstAndLast input) 
+
+
 
 
     [<Fact>]
@@ -135,10 +187,11 @@ module Input =
     let testsum2 () = 
         let input = readInit "input1.txt" 
         let sum = input |> Array.map (fun line -> firstAndLast line) |> Array.sum
-        Assert.Equal(53888, sum) 
+        Assert.Equal(53894, sum) 
 
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
         Assert.Equal(1000, input.Length) 
+
 module Program = let [<EntryPoint>] main _ = 0
