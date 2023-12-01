5173d5382f67b10a26631e14a65c4fb17142842c
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-01 07:07:16 +0100

    Read input

--- day_1/Program.fs
+++ day_1/Program.fs
@@ -3,15 +3,13 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let readInit (filePath: string): string array = 
+        System.IO.File.ReadAllLines filePath
+
 
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        Assert.Equal(1000, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
