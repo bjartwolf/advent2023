18c8eae8b5b69fa820b26e78c4800b6cf645e0db
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-01 10:20:52 +0100

    green and still no worky
    simplify

--- day_1/Program.fs
+++ day_1/Program.fs
@@ -1,9 +1,4 @@
-open System.Text.RegularExpressions
-
-
 module Input =
-    open System
-    open System.IO
     open Xunit 
 
     let readInit (filePath: string): string array = 
@@ -12,78 +7,92 @@ module Input =
     let numbers = ["zero", "0"; "one", "1"; "two", "2"; "three", "3"; "four", "4"; "five", "5"; "six", "6";"seven", "7"; "eight", "8"; "nine","9" ] 
     let numbersMap = numbers |> Map.ofList 
     let textNumbers = numbers |> List.map fst
+    let digits = numbers |> List.map snd 
 
-    let textNumberInLine (word: string): bool =
-        let matchingNumbers = textNumbers |> List.where (fun nr -> word.Contains(nr)) 
-        matchingNumbers|> List.isEmpty|> not
+    let findTxtNumberBy (comparer : int list -> int) (line: string): (int * string) option =
+        try 
+            let positions = textNumbers |> List.map (fun nr -> line.IndexOf(nr)) 
+            let positionsFound = positions |> List.where (fun x -> x >=0)
+            let minPositionFound = positionsFound |> comparer 
+            let indexOfMin = List.findIndex (fun x -> x = minPositionFound) positions
+            Some(minPositionFound, textNumbers[indexOfMin])
+        with _ ->
+            None
 
-    let replaceOnce (line: string) (textNr: string) (digit: string) =
-        let pos = line.IndexOf(textNr)
-        if (pos >= 0) then
-            line.Substring(0,pos) + digit + line.Substring(pos+textNr.Length)
-        else 
-            line
+    let firstTxtNumber (line: string): (int * string) option =
+        findTxtNumberBy List.min line 
 
-    let firstNumber (line: string): string =
-        let positions = textNumbers |> List.map (fun nr -> line.IndexOf(nr)) 
-        let positionsFound = positions |> List.where (fun x -> x >=0)
-        let minPositionFound = positionsFound |> List.min
-        let indexOfMin = List.findIndex (fun x -> x = minPositionFound) positions
-        textNumbers[indexOfMin] 
-
-    [<Fact>]
-    let testFirstNumber () = 
-        Assert.Equal("seven", firstNumber "sevenseven") 
+    let lastTxtNumber(line: string): (int * string) option =
+        findTxtNumberBy List.max line 
 
     [<Fact>]
-    let testFirstNumber2 () = 
-        Assert.Equal("seven", firstNumber "dfadsevenseven") 
+    let firstText() = 
+        let input = "7pqrstsixteen" 
+        Assert.Equal(Some (6,"six"), firstTxtNumber input) 
 
+    let findDigitNumberBy (comparer: int list -> int) (line: string): (int * string) option =
+        try 
+            let positions = digits |> List.map (fun nr -> line.IndexOf(nr)) 
+            let positionsFound = positions |> List.where (fun x -> x >=0)
+            let minPositionFound = positionsFound |> comparer 
+            let indexOfMin = List.findIndex (fun x -> x = minPositionFound) positions
+            Some (minPositionFound, digits[indexOfMin])
+        with _ ->
+            None
+
+
+    let firstDigitNumber (line: string): (int * string) option =
+        findDigitNumberBy List.min line
+
+    let lastDigitNumber (line: string): (int * string) option =
+        findDigitNumberBy List.max line
 
     [<Fact>]
-    let testreplace () = 
-        Assert.Equal("7seven", replaceOnce "sevenseven" "seven" "7") 
-        Assert.Equal("77", replaceOnce "7seven" "seven" "7") 
+    let firstDigitNumberTest() = 
+        let input = "7pqrstsixteen" 
+        Assert.Equal(Some (0,"7"), firstDigitNumber input) 
 
     [<Fact>]
-    let testreplace2 () = 
-        Assert.Equal("8wothree", replaceOnce "eightwothree" "eight" "8") 
-        Assert.Equal("8wo3", replaceOnce "8wothree" "three" "3") 
+    let firstDigitNumberTest2() = 
+        let input = "foo7pqrstsixteen" 
+        Assert.Equal(Some (3,"7"), firstDigitNumber input) 
+
+    [<Fact>]
+    let Lastdigittest1() = 
+        let input = "i8xteen" 
+        Assert.Equal(Some (1,"8"), lastDigitNumber input) 
 
     let firstAndLast (inputline: string): int=
-        let mutable line: string = inputline 
+        let firstDigit = firstDigitNumber inputline
+        let lastDigit = lastDigitNumber inputline
+        let firstTxt = firstTxtNumber inputline
+        let lastTxt = lastTxtNumber inputline
 
-        while (textNumberInLine line) do
-            let nrToReplace = firstNumber line 
-            line <- replaceOnce line nrToReplace (numbersMap.[nrToReplace])
-           
-        let mutable firstNumber : int option = None 
-        let mutable lastNumber: int option = None 
-        
-        for char in line do
-            ()
-            let mutable parsedValue = 0 
-            let couldParse = Int32.TryParse(string char, &parsedValue)
-            if couldParse then
-                if firstNumber.IsNone then
-                    firstNumber <- Some parsedValue
+        let firstNumber = match (firstDigit, firstTxt) with
+                                    | Some (i,x), Some (j,_) when i < j -> x 
+                                    | Some (i,_), Some (j,y) when i > j -> numbersMap.[y] 
+                                    | None, Some (j,y) -> numbersMap.[y] 
+                                    | Some(i,x), None -> x 
+                                    | _ -> failwith "oopsy"
 
-        let backWardsLine = String.Concat(Seq.rev line)
-        for char in backWardsLine do
-            ()
-            let mutable parsedValue = 0 
-            let couldParse = Int32.TryParse(string char, &parsedValue)
-            if couldParse then
-                if lastNumber.IsNone then
-                    lastNumber<- Some parsedValue
+        let lastNumber = match (lastDigit, lastTxt) with
+                                    | Some (i,x), Some (j,y) when i > j -> x 
+                                    | Some (i,x), Some (j,y) when i < j -> numbersMap.[y] 
+                                    | None, Some (j,y) -> numbersMap.[y] 
+                                    | Some(i,x), None -> x 
+                                    | _ -> failwith "oopsy"
 
-        match (firstNumber, lastNumber) with
-            | Some nr1, Some nr2  -> int (sprintf "%i%i" nr1 nr2)
-            | _,_ -> failwith "Did not find numbers"
+        int(firstNumber + lastNumber)
+
     [<Fact>]
     let testi1 () = 
         let input = "two1nine" 
         Assert.Equal(29, firstAndLast input) 
+
+    [<Fact>]
+    let testi1x () = 
+        let input = "twone" 
+        Assert.Equal(21, firstAndLast input) 
 
     [<Fact>]
     let testi2 () = 
@@ -126,7 +135,7 @@ module Input =
     let testsum2 () = 
         let input = readInit "input1.txt" 
         let sum = input |> Array.map (fun line -> firstAndLast line) |> Array.sum
-        Assert.Equal(53896, sum) 
+        Assert.Equal(53888, sum) 
 
     [<Fact>]
     let test2 () = 
