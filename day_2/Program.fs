cbcb1e82b5f2075681e48579a09df55b7afe45bd
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-02 07:34:50 +0100

    Update Program.fs

--- day_2/Program.fs
+++ day_2/Program.fs
@@ -15,6 +15,7 @@ module Input =
         Assert.Equal(100, input.Length) 
 
 module Advent = 
+  open Xunit 
   type Draw = { Red: int; Green: int; Blue: int}
   type Game = { Id: int; Draws: Draw list }
 
@@ -28,6 +29,23 @@ module Advent =
   let game3 = {Id = 3; Draws = [ { Green = 8; Blue = 6; Red = 20}; { Blue = 5; Red = 4; Green = 13}; { Green = 5; Red = 1; Blue = 0} ] }
   let game4 = {Id = 4; Draws = [ { Green = 1; Red = 3; Blue = 6 }; {Green = 3; Red = 6; Blue = 0}; {Green = 3; Blue = 15; Red = 14}] }
   let game5 = {Id = 5; Draws = [ { Red = 6; Blue = 1; Green = 3}; {Blue = 2; Red = 1; Green = 2}] }
+
+
+  let processGame (game: Game): GameResult =
+    let allGamesPass = game.Draws |> List.forall(fun draw -> match draw with
+                                         | draw when draw.Red <= maxCubes.Red && draw.Green <= maxCubes.Green && draw.Blue <= maxCubes.Blue -> true 
+                                         | _ -> false)
+    if allGamesPass then 
+      Some { Id = game.Id}
+    else 
+      None
+
+  [<Fact>]
+  let game1_is_possible() = 
+    let game = game1
+    let gamePassed = processGame game
+    Assert.Equal(game.Id, gamePassed.Value.Id)
+    
 
   // game 1, 2 og 5 er possible
 
