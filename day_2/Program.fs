864ad1c9cff002af2f26453c0f55886fd40e0852
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-02 07:21:00 +0100

    Update Program.fs

--- day_2/Program.fs
+++ day_2/Program.fs
@@ -15,17 +15,21 @@ module Input =
         Assert.Equal(100, input.Length) 
 
 module Advent = 
-  (*Each time, secret cubes in bg
-    Game is row
-   *)
-  //type Cube = Red | Green | Blue
   type Draw = { Red: int; Green: int; Blue: int}
   type Game = { Id: int; Draws: Draw list }
 
-  type PossibleGame = { Id: int}
+  type PossibleGame = { Id: int }
+  type GameResult = PossibleGame option 
 
-  let constraint = { Red = 12; Green = 13; Blue = 14}
+  let maxCubes = { Red = 12; Green = 13; Blue = 14}
 
+  let game1 = {Id = 1; Draws = [ { Blue = 3; Red = 4; Green = 0 } ; { Red = 1; Green = 2; Blue =6 }; { Green = 2; Red = 0; Blue = 1} ]}
+  let game2 = {Id = 2; Draws = [ { Blue = 1; Green = 2; Red = 0}; {Green = 3; Blue = 4; Red = 1}; { Green = 1; Blue = 1; Red = 0 }] }
+  let game3 = {Id = 3; Draws = [ { Green = 8; Blue = 6; Red = 20}; { Blue = 5; Red = 4; Green = 13}; { Green = 5; Red = 1; Blue = 0} ] }
+  let game4 = {Id = 4; Draws = [ { Green = 1; Red = 3; Blue = 6 }; {Green = 3; Red = 6; Blue = 0}; {Green = 3; Blue = 15; Red = 14}] }
+  let game5 = {Id = 5; Draws = [ { Red = 6; Blue = 1; Green = 3}; {Blue = 2; Red = 1; Green = 2}] }
+
+  // game 1, 2 og 5 er possible
 
   // sum of ids of games
 //  type Bag = 
