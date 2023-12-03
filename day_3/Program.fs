b8f6e51b05470ead508db82a0c7fbc2396256ac4
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-03 11:10:13 +0100

    make mask

--- day_3/Program.fs
+++ day_3/Program.fs
@@ -1,18 +1,11 @@
 module Input =
-    open System
-    open System.IO
     open Xunit 
-    (* 10x10 testinput, 140x140 real input, quadratic 
-    - add up parts numbers
-    - any number adjacent to a symbol, even diagonally, is a part number
-    - periods are not symbols
-   
-   - find all symbols and create a filter mask to map it with.
-   - a symbol and its neighbourhood gets a . can operate on charcodes.
-    *)
 
     let digitCharCodes = [0..9] |> Seq.map (fun x -> byte x + 48uy) |> Seq.toArray
     let dotCharCode = byte '.'
+
+    let readLinesAsTxt (filePath: string) : string[] = 
+        System.IO.File.ReadAllLines(filePath)
 
     let readInit (filePath: string) : byte [][] = 
         let lines = System.IO.File.ReadAllLines(filePath)
@@ -35,13 +28,44 @@ module Input =
     let keepSymbolsDeleteDots() =
         Assert.Equal(0uy, keepSymbols (byte '.'))
 
-    (* Sets symobls *)
-    let setSymbolMask (input: byte[][]): byte[][] =
-        input |> Array.map (fun x -> x |> Array.map (fun code -> code))
+    (* get symbols in the mask, no neighbourhood *)
+    let getSymbolMask (input: byte[][]): byte[][] =
+        input |> Array.map (fun x -> x |> Array.map keepSymbols)
+
+
+    let copy (input: byte[][]) =
+        input |> Array.map (fun x -> Array.copy x)
+
+    (* expand symbolmask to set all neighbours to 255uy *) 
+    // quadratic, so cheating
+    let getSymbolMaskNeighbors (input: byte[][]): byte[][] =
+         let newMask = copy input
+         let iterend = input.Length - 1
+         for i in 0..iterend do
+            for j in 0.. iterend do
+                if input[i][j] = 255uy then
+                    let x = [-1 .. 1]
+                    let y = [-1 .. 1]
+                    for deltaX in x do
+                        for deltaY in y do
+                            if i + deltaX > 0 && i + deltaX < iterend && j + deltaY > 0 && j + deltaY < iterend then
+                                newMask[i+deltaX][j+deltaY] <- 255uy
+         newMask 
+
+
+    let matrixToText (input: byte[][]): string[] = 
+        input |> Array.map (fun x -> System.String (x |> Seq.map char |> Seq.toArray))
 
     [<Fact>]
     let test2 () = 
         let input = readInit "input.txt" 
         Assert.Equal(140, input.Length) 
+
+    [<Fact>]
+    let checkText() = 
+        let txt = matrixToText (readInit "testinput.txt") |> Array.toList
+        let rawText = readLinesAsTxt "testinput.txt" |> Array.toList
+        let foo = matrixToText (getSymbolMaskNeighbors(getSymbolMask (readInit "testinput.txt")))
+        Assert.Equal<string list>(rawText, txt)
 
 module Program = let [<EntryPoint>] main _ = 0
