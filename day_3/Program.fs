b9f36953bade807f892b58e2d630cefe0142a1c9
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-03 10:44:45 +0100

    this could be fun

--- day_3/Program.fs
+++ day_3/Program.fs
@@ -2,16 +2,40 @@ module Input =
     open System
     open System.IO
     open Xunit 
+    (* 10x10 testinput, 140x140 real input, quadratic 
+    - add up parts numbers
+    - any number adjacent to a symbol, even diagonally, is a part number
+    - periods are not symbols
+   
+   - find all symbols and create a filter mask to map it with.
+   - a symbol and its neighbourhood gets a . can operate on charcodes.
+    *)
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let digitCharCodes = [0..9] |> Seq.map (fun x -> byte x + 48uy) |> Seq.toArray
+    let dotCharCode = byte '.'
+    let keep = 255uy
+    let delete = 0uy
+
+    let readInit (filePath: string) : byte [][] = 
+        let lines = System.IO.File.ReadAllLines(filePath)
+        lines |> Array.map (fun x -> x |> Seq.toArray |> Seq.map byte |> Seq.toArray)
+
+
+    let keepSymbols (char:byte) : byte =
+        if Seq.contains char digitCharCodes then 0uy
+        else char
+
+    [<Fact>]
+    let keepSymbolsDeleteNumbers() =
+        Assert.Equal(0uy, keepSymbols digitCharCodes.[3])
+
+    (* Sets symobls *)
+    let setSymbolMask (input: byte[][]): byte[][] =
+        input |> Array.map (fun x -> x |> Array.map (fun code -> code))
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let input = readInit "input.txt" 
+        Assert.Equal(140, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
