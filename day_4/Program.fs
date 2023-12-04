5d0ce2812182a09625520957b58e5f4ff944fdc4
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-04 06:15:11 +0100

    parse card

--- day_4/Program.fs
+++ day_4/Program.fs
@@ -6,6 +6,30 @@ module Input =
     let readInit (filePath: string) = 
         System.IO.File.ReadAllLines filePath
 
+    // first winning numbers, then numbers you have
+    // can be split on lines and newlines easier than regex
+    let example_line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    type Card = { Id: int ; Winning: int list; Scratched : int list}
+    let example_card = { Id = 1; Winning = [41;48;83;86;17]; Scratched = [83;86;6;31;17;9;48;53] } 
+
+    let parseCard (s:string): Card = 
+        let card_numbers = example_line.Split(":")
+        let nr = int (card_numbers[0].Replace("Card ",""))
+        let numbers = card_numbers[1].Split("|")
+        let parseNrs (numTxt:string[]) = numTxt |> Array.map (fun s -> s.Replace(" ", "")) |> Array.where (fun s -> not (String.IsNullOrEmpty(s))) |> Array.map int |> Array.toList
+        let winningNumbers = numbers[0].Split(" ")  |> parseNrs
+        let scracthed = numbers[1].Split(" ")  |> parseNrs
+        {
+            Id = nr 
+            Winning = winningNumbers;
+            Scratched = scracthed 
+        }
+
+    [<Fact>]
+    let parseLine_card1() = 
+        Assert.Equal(example_card, parseCard example_line)
+
+
     [<Fact>]
     let test2 () = 
         let input = readInit "testinput.txt" 
