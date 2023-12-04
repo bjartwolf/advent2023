827ad52aadb888baa025c78581c2cd5edd142046
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-04 07:21:05 +0100

    Update Program.fs

--- day_4/Program.fs
+++ day_4/Program.fs
@@ -1,14 +1,10 @@
 module Input =
     open System
-    open System.IO
     open Xunit 
 
     let readInit (filePath: string) = 
         System.IO.File.ReadAllLines filePath
 
-    // first winning numbers, then numbers you have
-    // can be split on lines and newlines easier than regex
-    let example_line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
     type CardId = CardId of int
     type Card = { Id: CardId ; Winning: int list; Scratched : int list}
     let example_card = { Id = CardId 1; Winning = [41;48;83;86;17]; Scratched = [83;86;6;31;17;9;48;53] } 
@@ -32,30 +28,10 @@ module Input =
     let parseCards (s: string[]): Card list = 
         s |> Array.map parseCard |> Array.toList
 
-    [<Fact>]
-    let parseLine_card1() = 
-        Assert.Equal(example_card, parseCard example_line)
-
     let countWinning (card: Card): CardId list =
         let nrWins = Set.intersect (Set.ofList card.Winning) (Set.ofList card.Scratched) |> Seq.toList |> Seq.length
         let nextIds = [(getCardValue card + 1) .. (getCardValue card + nrWins )] |> List.take nrWins
         nextIds |> List.map CardId 
-
-    [<Fact>]
-    let testCountWinning1() =
-        let cards = readInit "testinput.txt" |> parseCards 
-        let wins1 = countWinning cards[0]
-        let wins2 = countWinning cards[1]
-        let wins3 = countWinning cards[2]
-        let wins4 = countWinning cards[3]
-        let wins5 = countWinning cards[4]
-        let wins6 = countWinning cards[5]
-        Assert.Equal<CardId list>([2;3;4;5] |> List.map CardId, wins1)
-        Assert.Equal<CardId list>([3;4] |> List.map CardId, wins2)
-        Assert.Equal<CardId list>([4;5] |> List.map CardId, wins3)
-        Assert.Equal<CardId list>([5] |> List.map CardId, wins4)
-        Assert.Equal<CardId list>([] |> List.map CardId, wins5)
-        Assert.Equal<CardId list>([] |> List.map CardId, wins6)
 
     let getNextRound (cards: Card list) : CardId list =
         cards |> List.map countWinning  |> List.collect id
@@ -78,15 +54,4 @@ module Input =
         let winningCount = countScore input 
         Assert.Equal(30, winningCount)
 
-    [<Fact>]
-    let countWinningReal() =
-        let input = readInit "input.txt" |> parseCards
-        let winningCount = countScore input 
-        Assert.Equal(8467762, winningCount)
-
-
 module Program = let [<EntryPoint>] main _ = 0
-        let input = readInit "input.txt" |> parseCards
-        let winningCount = countScore input 
-        Assert.Equal(30, winningCount)
-
