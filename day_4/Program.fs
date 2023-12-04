d2c8d04bfeabdb850a20ad050c4be8bf89c68f51
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-04 07:56:19 +0100

    memoize

--- day_4/Program.fs
+++ day_4/Program.fs
@@ -1,3 +1,6 @@
+open System.Collections.Generic
+
+
 module Input =
     open System
     open Xunit 
@@ -5,12 +8,12 @@ module Input =
     let readInit (filePath: string) = 
         System.IO.File.ReadAllLines filePath
 
-    type CardId = CardId of int
+    type CardId = int
     type Card = { Id: CardId ; Winning: int list; Scratched : int list}
-    let example_card = { Id = CardId 1; Winning = [41;48;83;86;17]; Scratched = [83;86;6;31;17;9;48;53] } 
+    let example_card = { Id = 1; Winning = [41;48;83;86;17]; Scratched = [83;86;6;31;17;9;48;53] } 
 
-    let getCardValue (card: Card): int = match card.Id with | CardId value -> value
-    let getCardIdValue (id: CardId): int = match id with | CardId value -> value
+    let getCardValue (card: Card): int = card.Id 
+    let getCardIdValue (id: CardId): int = id 
 
     let parseCard (s:string): Card = 
         let card_numbers = s.Split(":")
@@ -20,7 +23,7 @@ module Input =
         let winningNumbers = numbers[0].Split(" ")  |> parseNrs
         let scracthed = numbers[1].Split(" ")  |> parseNrs
         {
-            Id = CardId nr 
+            Id = nr 
             Winning = winningNumbers;
             Scratched = scracthed 
         }
@@ -28,18 +31,38 @@ module Input =
     let parseCards (s: string[]): Card list = 
         s |> Array.map parseCard |> Array.toList
 
+    let memoize f =
+        let dict = Dictionary<_, _>();
+        fun c ->
+            let exist, value = dict.TryGetValue c
+            match exist with
+            | true -> value
+            | _ -> 
+                let value = f c
+                dict.Add(c, value)
+                value
+
     let countWinning (card: Card): CardId list =
         let nrWins = Set.intersect (Set.ofList card.Winning) (Set.ofList card.Scratched) |> Seq.toList |> Seq.length
         let nextIds = [(getCardValue card + 1) .. (getCardValue card + nrWins )] |> List.take nrWins
-        nextIds |> List.map CardId 
+        nextIds 
+
+    let countWinningById (cards: Card list) (cardId: CardId): CardId list =
+        let card = cards[cardId - 1 ]
+        let nrWins = Set.intersect (Set.ofList card.Winning) (Set.ofList card.Scratched) |> Seq.toList |> Seq.length
+        let nextIds = [(getCardValue card + 1) .. (getCardValue card + nrWins )] |> List.take nrWins
+        nextIds
+
+    let memoedWinning (cards: Card list) =
+        memoize (countWinningById cards)
 
     let getNextRound (cards: Card list) : CardId list =
         cards |> List.map countWinning  |> List.collect id
 
     let rec countCards (cards: Card list) (hand: CardId list) (score: int): int =
+        let memoCounter = memoedWinning cards
         let newScore = score + hand.Length 
-        let cardsOnHand = hand |> List.map getCardIdValue |> List.map (fun x -> cards[x - 1])
-        let nextWins = getNextRound cardsOnHand 
+        let nextWins = hand |> List.map (fun x -> memoCounter x) |> List.collect id
         if nextWins |> List.isEmpty then 
             newScore 
         else 
@@ -53,5 +76,12 @@ module Input =
         let input = readInit "testinput.txt" |> parseCards
         let winningCount = countScore input 
         Assert.Equal(30, winningCount)
+
+    [<Fact>]
+    let countWinningTest2() =
+        let input = readInit "input.txt" |> parseCards
+        let winningCount = countScore input 
+        Assert.Equal(8467762, winningCount)
+
 
 module Program = let [<EntryPoint>] main _ = 0
