6060ba98d79ecc111fdc8f4900265e5ecc4cef0b
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-05 06:50:33 +0100

    shitty parser

--- day_5/Program.fs
+++ day_5/Program.fs
@@ -1,17 +1,41 @@
+open System.Collections.Generic
+
+
 module Input =
     open System
     open System.IO
     open Xunit 
+    type line = { src: int; dst: int; rng: int}
+    let groupByWhiteSpace (input: string[]) : string list list =
+        let mutable tmpGroup: string list = []
+        let mutable readingGroup = false
+
+        [ for i = 0 to Array.length input - 1 do
+              let crntLine = input[i]
+              if (not readingGroup && crntLine.Contains(":")) then
+                tmpGroup <- [] 
+                readingGroup <- true
+              if readingGroup && crntLine <> "" then
+                tmpGroup <- tmpGroup @ [crntLine]
+              else
+                if crntLine = "" then
+                    readingGroup <- false
+                    yield tmpGroup
+        ] 
 
     let test_seeds = [79;14;55;13]
-    let readInit (filePath: string): int list = 
+    let readInit (filePath: string): (int list* line list list) = 
         let input = System.IO.File.ReadAllLines filePath
         let seeds = input[0].Replace("seeds: ","").Split(" ") |> Array.map (fun x -> int x) |> Array.toList
-        seeds 
+        let maps = input[2..] 
+        let bar = groupByWhiteSpace maps 
+        seeds, [[{src=1;dst=1;rng=1}]]
+
 
     [<Fact>]
     let test2 () = 
         let input = readInit "testinput.txt" 
-        Assert.Equal<int list>(test_seeds, input) 
+        let seeds,_= input
+        Assert.Equal<int list>(test_seeds, seeds) 
 
 module Program = let [<EntryPoint>] main _ = 0
