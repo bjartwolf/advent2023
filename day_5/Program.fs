dad13a79f482f7d2d508d5976741059e82f03cf5
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-05 06:24:36 +0100

    input seed

--- day_5/Program.fs
+++ day_5/Program.fs
@@ -3,15 +3,15 @@ module Input =
     open System.IO
     open Xunit 
 
+    let test_seeds = [79;14;55;13]
     let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+        let input = System.IO.File.ReadAllLines filePath
+        let seeds = input[0].Replace("seeds: ","").Split(" ") |> Array.map (fun x -> int x) |> Array.toList
+        seeds 
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let input = readInit "testinput.txt" 
+        Assert.Equal<int list>(test_seeds, input) 
 
 module Program = let [<EntryPoint>] main _ = 0
