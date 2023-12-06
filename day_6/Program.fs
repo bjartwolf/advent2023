bf9f8d5f2bd42cad9d04e84cbb07b037125ab4f9
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-06 08:19:34 +0100

    Update Program.fs

--- day_6/Program.fs
+++ day_6/Program.fs
@@ -13,8 +13,6 @@ module Input =
     let tts (races: race list) = races |> List.map (fun r -> r.tt)
     let ds (races: race list) = races |> List.map (fun r -> r.dist)
 
-//    let tp_possible (race_total_time: int64) = [0 .. race_total_time]
-
     let dist time_total time_pressed = { tp = time_pressed;
                                          dist_raced = time_pressed * (time_total - time_pressed)  }
 
@@ -24,18 +22,6 @@ module Input =
     let filter_win_times (attempts: attempt list) (record:int64) : attempt list =
         attempts |> List.filter (fun a -> a.dist_raced > record )
 
-        (*
-    let find_all_wintimes (r: race)  : int64 list =
-        let times = tp_possible r.tt 
-        let attempts = distances r.tt times
-        let filtered_distances = filter_win_times attempts  r.dist 
-        filtered_distances |> List.map (fun a -> a.tp)
-*)
-    // must be binary search to find the elements 
-    // find binary search, make work for long, make equal for testdata
-    // should calculate the same results
-
-    // search min is 0, search max is max, searching for smallest number that will win
     let findMin (r: race): int64 =
        let max_guess = r.tt
        
@@ -68,28 +54,9 @@ module Input =
                innerGuess newGuess
        innerGuess (max_guess / 2L - 1L)
 
-    [<Fact>]
-    let find_largest_binary() =
-         Assert.Equal(11L, findMax races_test[1])
-         Assert.Equal(5L, findMax races_test[0])
-         Assert.Equal(19L, findMax races_test[2])
-
-
-    [<Fact>]
-    let find_smallest_binary() =
-        Assert.Equal(4L, findMin races_test[1])
-        Assert.Equal(2L, findMin races_test[0])
-        Assert.Equal(11L, findMin races_test[2])
-
     let find_all_wintimes_binary (r: race)  : int64 =
- //       let wins = find_all_wintimes r 
-   //     let find_max_wintime = wins |> List.max 
         let find_max_wintime_binary = findMax r 
-     //   if find_max_wintime <> find_max_wintime_binary then failwith (sprintf "max is wrong real %A guess %A" find_max_wintime find_max_wintime_binary)
-    //    let find_min_wintime = wins |> List.min
-
         let find_min_wintime_binary = findMin r 
-  //      if find_min_wintime <> find_min_wintime_binary then failwith (sprintf "min is wrong real %A guess %A" find_min_wintime find_max_wintime_binary)
         find_max_wintime_binary - find_min_wintime_binary + 1L
 
     [<Fact>]
@@ -97,33 +64,7 @@ module Input =
         Assert.Equal(4L, find_all_wintimes_binary races_test[0] )
         Assert.Equal(8L, find_all_wintimes_binary races_test[1] )
         Assert.Equal(9L, find_all_wintimes_binary races_test[2] )
+        Assert.Equal(71503L, find_all_wintimes_binary race_large_test)
         Assert.Equal(36749103L, find_all_wintimes_binary race_large_real )
-
-//    let find_all_wintimes_count (r:race): int = 
-//        find_all_wintimes r |> List.length 
-
-(*
-    [<Fact>]
-    let test_time_pressed_list() =
-        Assert.Equal(4L, find_all_wintimes_count races_test[0] )
-        Assert.Equal(8L, find_all_wintimes_count races_test[1] )
-        Assert.Equal(9L, find_all_wintimes_count races_test[2] )
-*)
-
-    //[<Fact>]
-    //let test_time_pressed() =
-    //    let race_dist = dist 7
-    //    Assert.Equal(0, (race_dist 0).dist_raced)
-    //    Assert.Equal(6,( race_dist 1).dist_raced)
-    //    Assert.Equal(10,( race_dist 2).dist_raced)
-    //    Assert.Equal(12,( race_dist 3).dist_raced)
-    //    Assert.Equal(0,( race_dist 7).dist_raced)
- 
-    //[<Fact>]
-    //let test2 () =
-    //    Assert.Equal(3,races_test.Length)
-    //    Assert.Equal<int list>([7;15;30], tts races_test)
-    //    Assert.Equal<int list>([9;40;200], ds races_test)
-    //    Assert.Equal<int list>([0;1;2;3;4;5;6;7], tp_possible 7)
 
  module Program = let [<EntryPoint>] main _ = 0
