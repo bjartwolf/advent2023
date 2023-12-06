bb72c23d4c319c37f9c5541e19011b8d20d2ca24
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-06 06:25:42 +0100

    Update Program.fs

--- day_6/Program.fs
+++ day_6/Program.fs
@@ -4,7 +4,9 @@ module Input =
     open Xunit 
 
     type race = { tt: int;dist: int}
-    let races_test = [ {tt=7;dist=9};{tt=15;dist=40};{tt=30;dist=200} ]
+    let races_test = [ {tt=7;dist=9};
+                       {tt=15;dist=40};
+                       {tt=30;dist=200} ]
 
     let tts (races: race list) = races |> List.map (fun r -> r.tt)
     let ds (races: race list) = races |> List.map (fun r -> r.dist)
@@ -12,7 +14,7 @@ module Input =
     [<Fact>]
     let test2 () =
         Assert.Equal(3,races_test.Length)
-        Assert.Equal<int list>([7;15;40], tts races_test)
+        Assert.Equal<int list>([7;15;30], tts races_test)
         Assert.Equal<int list>([9;40;200], ds races_test)
 
 module Program = let [<EntryPoint>] main _ = 0
