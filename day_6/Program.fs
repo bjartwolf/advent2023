9a936f66c8590b7c8710ff66739d8cd3beb373d7
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-06 14:48:44 +0100

    Update Program.fs

--- day_6/Program.fs
+++ day_6/Program.fs
@@ -1,15 +1,13 @@
 module Program =
     open System
-    type race = { tt: int64;dist: int64}
 
     let solve b' c' sum =
         let (b,c) = float b', float c'
         (sum -b (Math.Sqrt(b*b-4.0*c))/(2.0))
 
-    let find r = 
-        int64 (Math.Ceiling(solve -r.tt r.dist (+)) - (Math.Floor(solve -r.tt r.dist (-)))) - 1L
+    let find t d = int64 (Math.Ceiling(solve -t d (+)) - (Math.Floor(solve -t d (-)))) - 1L
 
     let [<EntryPoint>] main _ =
         printfn "Answer should be %A" 36749103 
-        printfn "Answer is        %A" (find { tt= 48938466L; dist = 261119210191063L} )
+        printfn "Answer is        %A" (find 48938466L 261119210191063L )
         0
