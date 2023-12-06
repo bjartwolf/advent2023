4b7d2a652daab5750853369112564c6b306757fc
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-06 11:56:53 +0100

    Update Program.fs

--- day_6/Program.fs
+++ day_6/Program.fs
@@ -1,20 +1,15 @@
-open System
-
-
-module Input =
-    open Xunit 
+module Program =
+    open System
     type race = { tt: int64;dist: int64}
-    let race_large_real = { tt= 48938466L; dist = 261119210191063L}
 
-    let solve (b':int64) (c':int64) (sum: float->float->float)=
+    let solve b' c' sum =
         let (b,c) = float b', float c'
         (sum -b (Math.Sqrt(b*b-4.0*c))/(2.0))
 
-    let find_all_wintimes_binary (r: race)  : int64 =
+    let find r = 
         int64 (Math.Ceiling(solve -r.tt r.dist (+)) - (Math.Floor(solve -r.tt r.dist (-)))) - 1L
 
-    [<Fact>]
-    let binary_is_equal() =
-        Assert.Equal(36749103L, find_all_wintimes_binary race_large_real )
-
- module Program = let [<EntryPoint>] main _ = 0
+    let [<EntryPoint>] main _ =
+        printfn "Answer should be %A" 36749103 
+        printfn "Answer is        %A" (find { tt= 48938466L; dist = 261119210191063L} )
+        0
