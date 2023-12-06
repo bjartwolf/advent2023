a0d4363708a3a34d060e45ca707b54d80febc780
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-06 14:54:52 +0100

    Update Program.fs

--- day_6/Program.fs
+++ day_6/Program.fs
@@ -1,13 +1,11 @@
 module Program =
     open System
 
-    let solve b' c' sum =
-        let (b,c) = float b', float c'
-        (sum -b (Math.Sqrt(b*b-4.0*c))/(2.0))
+    let solve b c sum = (sum -b (Math.Sqrt(b*b-4.0*c))/(2.0))
 
     let find t d = int64 (Math.Ceiling(solve -t d (+)) - (Math.Floor(solve -t d (-)))) - 1L
 
     let [<EntryPoint>] main _ =
         printfn "Answer should be %A" 36749103 
-        printfn "Answer is        %A" (find 48938466L 261119210191063L )
+        printfn "Answer is        %A" (find 48938466.0 261119210191063.0)
         0
