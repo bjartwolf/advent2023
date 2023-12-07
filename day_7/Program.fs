1b9058b29c74254f7c156affe3ebc4fb2249a51f
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-07 06:51:16 +0100

    Update Program.fs

--- day_7/Program.fs
+++ day_7/Program.fs
@@ -1,7 +1,7 @@
-module Input =
+module Program =
     open System
-    open System.IO
     open Xunit 
+
     // lowerranked ASCII, remember when sorting by value
     // T=:, J=;, Q=< K = = , A= >
     // T=58, J=59, Q=60, K=61 A=62
@@ -22,10 +22,13 @@ module Input =
             |> Array.map (fun x -> let a = x.Split(" ")
                                    (replaceWithAsciiValues a[0]),int a[1])
 
+    //let countEqual (hand: string) -> 
+    let testinput = readInit "testinput.txt" 
+        
+
     [<Fact>]
     let test2 () = 
-        let input = readInit "testinput.txt" 
-        Assert.Equal(5, input.Length) 
-        Assert.Equal(("32:3=",765), input[0])
+        Assert.Equal(5, testinput.Length) 
+        Assert.Equal(("32:3=",765), testinput[0])
 
-module Program = let [<EntryPoint>] main _ = 0
+    let [<EntryPoint>] main _ = 0
