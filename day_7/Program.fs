afc8f8e8bf5efe459e8ffd7c2e9632bf0bb2b98f
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-07 07:02:47 +0100

    Update Program.fs

--- day_7/Program.fs
+++ day_7/Program.fs
@@ -24,8 +24,19 @@ module Program =
 
     //let countEqual (hand: string) -> 
     let testinput = readInit "testinput.txt" 
-        
+       
+    // -1 means hand1 is weaker than hand2
+    // 1 means hand1 is stronger than hand2
+    // a string here startes with the highest ascii car codes for strongest card
+    let compareRule2 ((hand1,_):string*int) ((hand2,_):string*int): int =
+        hand1.CompareTo(hand2) 
 
+    [<Fact>]
+    let testRule2 () = 
+        Assert.Equal(0, compareRule2 testinput[0] testinput[0]) 
+        Assert.Equal(1, compareRule2 testinput[2] testinput[3]) // Card 2 is stronger than card 3 because KK > KT 
+        Assert.Equal(-1, compareRule2 testinput[1] testinput[4]) // Card 4 is stronger than 1 
+ 
     [<Fact>]
     let test2 () = 
         Assert.Equal(5, testinput.Length) 
