e0491718ddf9899eddba37f62cb2e119cd2abfbb
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-10 10:43:20 +0100

    Update Program.fs

--- day_10/Program.fs
+++ day_10/Program.fs
@@ -1,4 +1,4 @@
-module Input =
+﻿module Input =
     open System
     open System.IO
     open Xunit 
@@ -60,49 +60,101 @@ module Input =
         Assert.Equal<Position>((2,0), startPosition)
 
 
-    let move (direction: Direction) ((x,y): Position): Position  =
+    let move (direction: Direction) ((y,x): Position): Position  =
         match direction with 
-            | N -> (x,y+1)
-            | W -> (x-1,y)
-            | E -> (x+1,y)
-            | S -> (x,y-1)
+            | N -> (y-1,x)
+            | W -> (y,x-1)
+            | E -> (y,x+1)
+            | S -> (y+1,x)
 
-    let nextPosition (pipe: Pipe) (position: Position ) (direction: Direction): Position * Direction =
-        match pipe, direction with 
+    let nextPosition (pipe: Pipe) (position: Position ) (enteringDirection: Direction): Position * Direction =
+        match pipe, enteringDirection with 
+            // the straights
             | NS, S -> move S position, S 
             | NS, N -> move N position, N 
             | EW, E -> move E position, E 
             | EW, W -> move W position, W 
-            | NE, S -> move S position, E 
-            | NE, W -> move W position, N  
-            | NW, S -> move S position, W 
-            | NW, E -> move E position, N  
-            | SW, N -> move N position, W
-            | SW, E -> move E position, S
-            | SE, N -> move N position, E
-            | SE, W -> move W position, S 
-            | _ -> failwithf "%A %A does not work" pipe direction 
+            // the bends
+            // └ 
+            | NE, S -> move E position, E 
+            | NE, W -> move N position, N  
+            //  ┘
+            | NW, S -> move W position, W 
+            | NW, E -> move N position, N  
+            //  ┐
+            | SW, N -> move W position, W
+            | SW, E -> move S position, S
+            //  ┍ 
+            | SE, N -> move E position, E
+            | SE, W -> move S position, S 
+            | _ -> failwithf "%A %A does not work" pipe enteringDirection 
 
     let findStartDir (position: Position) (map: Map<Position,Pipe>): Direction =
         // check north
-        let northTile = Map.tryFind (move N position) map 
-        let southTile = Map.tryFind (move S position) map
-        let eastTile = Map.tryFind (move E position) map 
-        let westTile = Map.tryFind(move W position) map 
+        let north = move N position
+        let south = move S position
+        let east = move E position
+        let west = move W position
+        let northTile = Map.tryFind north map 
+        let southTile = Map.tryFind south map
+        let eastTile = Map.tryFind east map 
+        let westTile = Map.tryFind west map 
         match northTile, southTile, eastTile, westTile with
             | Some n,_,_,_ when n = NS || n = SW || n = SE -> N 
             | _,Some s,_,_ when s = NS || s = NW || s = NE -> S 
             | _,_,Some e,_ when e = EW || e = NW || e = SW -> E 
             | _,_,_,Some w when w = EW || w = NE || w = SE -> W 
             | _ -> failwithf "There has to be a direction from %A" position
-        
 
+    [<Fact>]
+    let testStartFindDirectionTest1() = 
+        let input = readInit "testinput1.txt" 
+        let pipeMap,startPosition = parsePipeMap input
+        let startDirection = findStartDir startPosition pipeMap 
+        Assert.Equal<Direction>(S, startDirection)
+
+    [<Fact>]
+    let testStartFindDirectionTest2() = 
+        let input = readInit "testinput2.txt" 
+        let pipeMap,startPosition = parsePipeMap input
+        let startDirection = findStartDir startPosition pipeMap 
+        Assert.Equal<Direction>(S, startDirection)
+
+    [<Fact>]
+    let testStartFindDirectionInput() = 
+        let input = readInit "input.txt" 
+        let pipeMap,startPosition = parsePipeMap input
+        let startDirection = findStartDir startPosition pipeMap 
+        Assert.Equal<Direction>(N, startDirection)
+          
+    let walkMap (startPosition: Position) (map: PipeMap): int =
+        let rec walkMapInner (steps: int) (position: Position) (direction: Direction) =
+            if position = startPosition then steps
+            else 
+                // I am at 2,1 going south
+                let pipe = Map.find position map
+                // hitting a pipe going south
+                let nextPosition, nextDirection = nextPosition pipe position direction
+                // then I end up in 3,1, entering direction is south
+                walkMapInner (steps + 1) nextPosition nextDirection
+        let startDirection = findStartDir startPosition map
+        let nextPosition = move startDirection startPosition 
+        let steps = walkMapInner 1 nextPosition startDirection 
+        steps / 2 // or something
+
+    [<Fact>]
+    let testWalkMap1() = 
+        let input = readInit "testinput1.txt" 
+        let pipeMap,startPosition = parsePipeMap input
+        ()
+        //let steps = walkMap startPosition pipeMap
+        //Assert.Equal(8, steps)
 
 
     [<Fact>]
     let testDirectionSEGoingW() = 
         let nextPosition, nextDirection = nextPosition SE (3,4) W
-        Assert.Equal<Position>( (2,4), nextPosition)
+        Assert.Equal<Position>( (4,4), nextPosition)
         Assert.Equal<Direction>(S, nextDirection) 
 
     [<Fact>]
@@ -114,7 +166,7 @@ module Input =
     [<Fact>]
     let testDirectionSWGoingN() = 
         let nextPosition, nextDirection = nextPosition SW (3,4) N
-        Assert.Equal<Position>( (3,5), nextPosition)
+        Assert.Equal<Position>( (3,3), nextPosition)
         Assert.Equal<Direction>(W, nextDirection) 
 
     [<Fact>]
@@ -126,13 +178,13 @@ module Input =
     [<Fact>]
     let testDirectionEWGoingE() = 
         let nextPosition, nextDirection = nextPosition EW (3,4) E
-        Assert.Equal<Position>( (4,4), nextPosition)
+        Assert.Equal<Position>( (3,5), nextPosition)
         Assert.Equal<Direction>(E, nextDirection) 
 
     [<Fact>]
     let testDirectionEWGoingW() = 
         let nextPosition, nextDirection = nextPosition EW (3,4) W
-        Assert.Equal<Position>( (2,4), nextPosition)
+        Assert.Equal<Position>( (3,3), nextPosition)
         Assert.Equal<Direction>(W, nextDirection) 
 
     [<Fact>]
@@ -144,7 +196,7 @@ module Input =
     [<Fact>]
     let testDirectionNEGoingS() = 
         let nextPosition, nextDirection = nextPosition NE (3,4) S
-        Assert.Equal<Position>( (3,3), nextPosition)
+        Assert.Equal<Position>( (3,5), nextPosition)
         Assert.Equal<Direction>(E, nextDirection) 
   
     [<Fact>]
@@ -155,8 +207,8 @@ module Input =
 
     [<Fact>]
     let testDirectionNWGoingE() = 
-        let nextPosition, nextDirection = nextPosition NW (3,4) E
-        Assert.Equal<Position>( (4,4), nextPosition)
+        let nextPosition, nextDirection = nextPosition NW (2,5) E
+        Assert.Equal<Position>( (1,5), nextPosition)
         Assert.Equal<Direction>(N, nextDirection) 
     
     [<Fact>]
