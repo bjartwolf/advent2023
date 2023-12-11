9b4cad18d1e637153aef036f826a62102d338439
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-11 06:23:29 +0100

    Update Program.fs

--- day_11/Program.fs
+++ day_11/Program.fs
@@ -3,15 +3,20 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let readInit (filePath: string): (int*int) list = 
+        let lines = IO.File.ReadAllLines(filePath)
+        [
+            let height = lines.Length
+            let width = lines[0].Length
+            for i in 0 .. height - 1 do
+                for j in 0 .. width - 1 do
+                    if lines[i][j] = '#' then
+                        yield (i,j)
+        ]
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let input = readInit "testinput.txt" 
+        Assert.Equal(9, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
