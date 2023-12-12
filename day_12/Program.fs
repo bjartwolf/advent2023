ec2e115c3d18e89a5d595342e9b235b884123d3c
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-12 06:38:22 +0100

    Update Program.fs

--- day_12/Program.fs
+++ day_12/Program.fs
@@ -3,8 +3,40 @@ module Input =
     open System.IO
     open Xunit 
 
+    let q = '?'
+
     let readInit (filePath: string): string []= 
         IO.File.ReadAllLines(filePath)
+
+    // strategi 1, lage alle komboer, filtrere ut 
+    // bruke mengder
+    // strategi 2, regulært uttrykk
+    // compile noe catchgroups, blir fort vanskelig.
+
+    // 2 i n antall spørsmålstegn, men det er ikke så mange ? i input
+
+    let rec findAllCombos (x:string) : string seq =
+        seq {
+            let index = x.IndexOf(q)
+            if index <> -1 then
+                let prefix = x.Substring(0, index)
+                let suffix = x.Substring(index + 1)
+                let replacedStringWithDot = prefix + "." + suffix
+                let replacedStringWithHash = prefix + "#" + suffix
+                yield! findAllCombos replacedStringWithDot
+                yield! findAllCombos replacedStringWithHash
+            else yield x
+        }
+
+    let findAll x =
+        findAllCombos x |> Set.ofSeq
+
+    [<Fact>]
+    let yieldCombinationsTest() = 
+        let test = "??"
+        let fasit = ["..";".#";"#.";"##"] |> Set.ofList
+        let combos = findAll test
+        Assert.Equal<Set<string>>(fasit, combos)
 
     [<Fact>]
     let test2 () = 
