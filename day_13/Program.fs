74e5ff0a35935284f9e210d17648a46e14ea6e48
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-13 18:51:21 +0100

    make matrixes

--- day_13/Program.fs
+++ day_13/Program.fs
@@ -1,19 +1,29 @@
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+
+
 module Input =
     open System
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): string list list = 
+    let readInit (filePath: string): string [] []= 
         let allLines = IO.File.ReadAllText(filePath)
         let boards = allLines.Split(Environment.NewLine+Environment.NewLine) 
         boards 
             |> Array.map (fun b -> b.Split(Environment.NewLine)) 
-            |> Array.map Array.toList
-            |> Array.toList
+
+    let getMatrixes (filePath:string) : Matrix<float> array =
+        let rawData = readInit filePath
+
+        rawData |> Array.map (
+            fun x -> x |> Array.map (fun a -> a.ToCharArray() |> Array.map ( fun c -> if c = '#' then 1.0 else 0.0))
+                       |> matrix )
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "testinput.txt" 
+        let input = getMatrixes "testinput.txt" 
+        printfn "%A" input
         Assert.Equal(2, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
