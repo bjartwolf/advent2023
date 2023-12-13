5caa51199ec6db800ff35ea1933ea8041fc7bbe8
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-13 06:18:19 +0100

    parse

--- day_13/Program.fs
+++ day_13/Program.fs
@@ -3,15 +3,17 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let readInit (filePath: string): string list list = 
+        let allLines = IO.File.ReadAllText(filePath)
+        let boards = allLines.Split(Environment.NewLine+Environment.NewLine) 
+        boards 
+            |> Array.map (fun b -> b.Split(Environment.NewLine)) 
+            |> Array.map Array.toList
+            |> Array.toList
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let input = readInit "testinput.txt" 
+        Assert.Equal(2, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
