c761761981b41e6ffcca1e0a98f88c07542f1c5d
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-15 06:38:55 +0100

    part 1

--- day_15/Program.fs
+++ day_15/Program.fs
@@ -7,7 +7,7 @@ module Input =
         let line = File.ReadAllLines filePath
                         |> Array.head
         line.Split(",")
-        
+            |> Array.toList
         
     let testInput = readInit "testinput.txt"
 
@@ -18,10 +18,30 @@ module Input =
     let makeWordTest () = 
         Assert.Equal<int list>([72;65;83;72],wordToNumbers "HASH")
 
-  // csv, ignore newline.
-  // blir masse tall i en linje
-  // sum av er et tall
-  // sum av det igjen
+    let calcWordNr (word: string) : int =
+        let rec calcWordNrInner (wordDigits: int list) (current: int): int =
+            match wordDigits with
+                | [] -> current
+                | h::t -> calcWordNrInner t (((current + h) * 17) % 256)
+        calcWordNrInner (wordToNumbers word) 0 
+
+    [<Fact>]
+    let calcWordTest () = 
+        Assert.Equal(52, calcWordNr "HASH")
+            
+    let calcWordList (words: string list) : int list =
+        words |> List.map calcWordNr
+
+    [<Fact>]
+    let calcWordListTest () = 
+        let input = readInit "testinput.txt" 
+        Assert.Equal<int list>([30;253;97;47;14;180;9;197;48;214;231], calcWordList input)
+
+    [<Fact>]
+    let calcSumForTest () = 
+        let input = readInit "testinput.txt" 
+        Assert.Equal(1320, input |> calcWordList |> List.sum) 
+
 
     [<Fact>]
     let test2 () = 
