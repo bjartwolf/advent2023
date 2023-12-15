4b3edfcf987a825b8df851f5849a6f2bbf9b4b85
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-15 06:26:34 +0100

    Update Program.fs

--- day_15/Program.fs
+++ day_15/Program.fs
@@ -3,20 +3,29 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let readInit (filePath: string) = 
+        let line = File.ReadAllLines filePath
+                        |> Array.head
+        line.Split(",")
         
-    let testString = "HASH"
+        
+    let testInput = readInit "testinput.txt"
+
+    let wordToNumbers (word:string): int list =
+        word.ToCharArray() |> Array.map int |> Array.toList
+
+    [<Fact>]
+    let makeWordTest () = 
+        Assert.Equal<int list>([72;65;83;72],wordToNumbers "HASH")
+
   // csv, ignore newline.
   // blir masse tall i en linje
   // sum av er et tall
   // sum av det igjen
+
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let input = readInit "input.txt" 
+        Assert.Equal(4000, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
