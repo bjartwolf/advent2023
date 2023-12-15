e27669cde4ad2c6f4350080d1b058a3a65ad34ea
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-15 09:52:24 +0100

    part 2
    part 2

--- day_15/Program.fs
+++ day_15/Program.fs
@@ -1,5 +1,4 @@
 module Input =
-    open System
     open System.IO
     open Xunit 
 
@@ -14,10 +13,6 @@ module Input =
     let wordToNumbers (word:string): int list =
         word.ToCharArray() |> Array.map int |> Array.toList
 
-    [<Fact>]
-    let makeWordTest () = 
-        Assert.Equal<int list>([72;65;83;72],wordToNumbers "HASH")
-
     let hashLabel (word: string) : int =
         let rec calcWordNrInner (wordDigits: int list) (current: int): int =
             match wordDigits with
@@ -25,36 +20,21 @@ module Input =
                 | h::t -> calcWordNrInner t (((current + h) * 17) % 256)
         calcWordNrInner (wordToNumbers word) 0 
 
-    [<Fact>]
-    let calcWordTest () = 
-        Assert.Equal(52, hashLabel "HASH")
-        Assert.Equal(0, hashLabel "rn")
-        Assert.Equal(0, hashLabel "cm")
-        Assert.Equal(1, hashLabel "qp")
-        Assert.Equal(3, hashLabel "pc")
-        Assert.Equal(3, hashLabel "ot")
-            
     let calcWordList (words: string list) : int list =
         words |> List.map hashLabel 
 
-    [<Fact>]
-    let calcWordListTest () = 
-        let input = readInit "testinput.txt" 
-        Assert.Equal<int list>([30;253;97;47;14;180;9;197;48;214;231], calcWordList input)
-
-    [<Fact>]
-    let calcSumForTest () = 
-        let input = readInit "testinput.txt" 
-        Assert.Equal(1320, input |> calcWordList |> List.sum) 
-
-    let focalLengths = [1 .. 9]
     type LabeledLens = string*int
     type Command = Remove of string | Add of LabeledLens 
     type LabeledLenses = LabeledLens list
     type Boxes = Map<int,LabeledLenses> 
-    
-    // can maybe just do this with a custom hash function... But there is some
-    // weirdness with the list and ordering and focal length stuff
+
+    let parseCommand (cmd: string): Command =
+        if cmd.Contains('-') then
+            let label = cmd.Replace("-","")
+            Remove label 
+        else
+            let split= cmd.Split("=")
+            Add (split[0],int split[1])
 
     let removeFromBoxes (boxes: Boxes) ((label,_): LabeledLens): Boxes =
         let hash = hashLabel label
@@ -70,36 +50,54 @@ module Input =
         match findLenses with
             | None -> Map.add hash [(label,focalLength)] boxes
             | Some lenses -> if (lenses |> List.exists (fun (l,_) -> l = label))  then
-                                // there is one with same label, replace it 
                                 let updatedLenses = lenses |> List.map (fun (l,f) -> if l = label then (label,focalLength) else (l,f)) 
                                 Map.add hash updatedLenses boxes
                              else
-                                // nothing in the same box, add to end
                                 Map.add hash (lenses @ [(label,focalLength)]) boxes
-    [<Fact>]
-    let testAddingBoxes2() = 
-        let boxes: Boxes = Map.ofList [0, [("rn",1);("cm",2)] ]
-        let newBox = addToBoxes boxes ("rn",3)
-        Assert.Equal<Boxes>(Map.ofList [0, [("rn",3);("cm",2))];], newBox)
 
+    let parseCommands (fileName: string): Command list = 
+        readInit fileName 
+            |> List.map parseCommand
+
+    let processCommands (commands: Command list): Boxes = 
+        let rec processCommandsInner (commands: Command list) (box: Boxes) =
+           match commands with
+                | [] -> box
+                | h ::t -> match h with
+                                | Remove label -> processCommandsInner t (removeFromBoxes box (label,0)) // can simplify this
+                                | Add (label,lense) -> processCommandsInner t (addToBoxes box (label, lense) )
+        processCommandsInner commands Map.empty 
+
+    let focusPowerForLens (lenses: LabeledLenses): int =
+        lenses |> List.mapi (fun i (_,nr) -> nr*(i+1)) |> List.sum
+
+    let findFocusPower (boxes: Boxes) : int =
+        boxes |> Map.map (fun i lenses -> (i+1)* focusPowerForLens lenses) 
+              |> Map.values
+              |> Seq.sum
+    [<Fact>]
+    let test_procsessTestDataSum() = 
+        let cmds = parseCommands "testinput.txt" 
+        let processedCommands = processCommands cmds 
+        let sum = findFocusPower processedCommands
+        Assert.Equal(145, sum)
 
     [<Fact>]
-    let testAddingBoxes() = 
-        let boxes: Boxes = Map.ofList [0, [("rn",1)] ]
-        let newBox = addToBoxes boxes ("qp",3)
-        Assert.Equal<Boxes>(Map.ofList [0, [("rn",1)]; 1, [("qp",3)]], newBox)
-        let nextBox = removeFromBoxes boxes ("qp",3)
-        Assert.Equal<Boxes>(Map.ofList [0, [("rn",1)]], nextBox)
-        Assert.Equal<Boxes>(Map.ofList [0,[]], removeFromBoxes nextBox ("rn",1))
+    let test_procsessDataSum() = 
+        let cmds = parseCommands "input.txt" 
+        let processedCommands = processCommands cmds 
+        let sum = findFocusPower processedCommands
+        Assert.Equal(303404, sum)
+  
+    [<Fact>]
+    let test_procsessTestData() = 
+        let cmds = parseCommands "testinput.txt" 
+        let processedCommands = processCommands cmds 
+        Assert.Equal<Boxes>(Map.ofList [ 0, [("rn",1); ("cm",2)];1,[]; 3,[("ot",7);("ab",5);("pc",6)]], processedCommands) 
 
     [<Fact>]
     let calcSumForReal () = 
         let input = readInit "input.txt" 
         Assert.Equal(518107, input |> calcWordList |> List.sum) 
-
-    [<Fact>]
-    let test2 () = 
-        let input = readInit "input.txt" 
-        Assert.Equal(4000, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
