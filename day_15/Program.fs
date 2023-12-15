2a6754088357d6189ecdbcf5d657e68a9597fb24
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-15 07:29:45 +0100

    Update Program.fs

--- day_15/Program.fs
+++ day_15/Program.fs
@@ -18,7 +18,7 @@ module Input =
     let makeWordTest () = 
         Assert.Equal<int list>([72;65;83;72],wordToNumbers "HASH")
 
-    let calcWordNr (word: string) : int =
+    let hashLabel (word: string) : int =
         let rec calcWordNrInner (wordDigits: int list) (current: int): int =
             match wordDigits with
                 | [] -> current
@@ -27,10 +27,15 @@ module Input =
 
     [<Fact>]
     let calcWordTest () = 
-        Assert.Equal(52, calcWordNr "HASH")
+        Assert.Equal(52, hashLabel "HASH")
+        Assert.Equal(0, hashLabel "rn")
+        Assert.Equal(0, hashLabel "cm")
+        Assert.Equal(1, hashLabel "qp")
+        Assert.Equal(3, hashLabel "pc")
+        Assert.Equal(3, hashLabel "ot")
             
     let calcWordList (words: string list) : int list =
-        words |> List.map calcWordNr
+        words |> List.map hashLabel 
 
     [<Fact>]
     let calcWordListTest () = 
@@ -42,6 +47,41 @@ module Input =
         let input = readInit "testinput.txt" 
         Assert.Equal(1320, input |> calcWordList |> List.sum) 
 
+        // focal lenth fra 1 - 9
+    let focalLengths = [1 .. 9]
+    type LabeledLens = string*int
+    type Command = Remove of string | Add of LabeledLens 
+    type LabeledLenses = LabeledLens list
+    type Boxes = Map<int,LabeledLenses> 
+    let boxes: Boxes = Map.empty
+
+    let removeFromBoxes (boxes: Boxes) ((label,focalLength): LabeledLens): Boxes =
+        let hash = hashLabel label
+        let findLenses = Map.tryFind hash boxes 
+        match findLenses with
+              | Some lenses -> let updated  = lenses |> List.filter (fun (l,focalLength) -> l <> label)
+                               Map.add hash updated boxes
+              | None -> boxes 
+
+    let addToBoxes (boxes: Boxes) ((label,focalLength): LabeledLens): Boxes =
+        let hash = hashLabel label
+        let existsSameLabel = Map.exists (fun x y -> x = hash && y |> List.contains (label,focalLength)) boxes
+        if existsSameLabel then 
+            boxes 
+        else
+            boxes 
+
+    (* 
+        REMOVE LABEL | ADD LABEL FOCALLENGTH
+        KEY = HASH(LABEL)
+        VALUE = LABEL,FOCALLENGTH
+        
+    *)
+
+    [<Fact>]
+    let calcSumForReal () = 
+        let input = readInit "input.txt" 
+        Assert.Equal(518107, input |> calcWordList |> List.sum) 
 
     [<Fact>]
     let test2 () = 
