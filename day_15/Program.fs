425462b0e354da2522087c55bf496663917039bc
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-15 06:20:53 +0100

    init

--- day_15/Program.fs
+++ day_15/Program.fs
@@ -8,7 +8,12 @@ module Input =
         let line = sr.ReadLine()
         let numbers = line.Split(",")
         numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
-
+        
+    let testString = "HASH"
+  // csv, ignore newline.
+  // blir masse tall i en linje
+  // sum av er et tall
+  // sum av det igjen
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
