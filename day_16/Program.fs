fbdf5e004c23d78f243832843681de944fe0233e
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-16 07:48:17 +0100

    Update Program.fs

--- day_16/Program.fs
+++ day_16/Program.fs
@@ -19,7 +19,30 @@ module Input =
         [for line in m do
             line |> String.Concat |> printfn "%A"
         ]
+    
+    let (+) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+    // alle posisjoner i y, der y er negativ akse og x
+    let North = (-1,0)
+    let South = (1,0)
+    let East = (0,1)
+    let West = (0,-1)
+    type Direction = N|S|E|W
+    type Position = int*int // kan lage record 
+    type Beam = Position * Direction 
+    type Beams = Beam list
 
+    let move ((p,d): Beam): Beam =
+        match d with 
+            | N -> (p+North, d) 
+            | E -> (p+East, d) 
+            | W -> (p+West, d) 
+            | S -> (p+South, d) 
+
+    [<Fact>]
+    let testMove() = 
+        Assert.Equal<Beam>(((2,1),S), move ((1,1),S) )
+        Assert.Equal<Beam>(((0,1),N), move ((1,1),N) )
+ 
     [<Fact>]
     let test2 () = 
         let map = readInit "testinput.txt" 
