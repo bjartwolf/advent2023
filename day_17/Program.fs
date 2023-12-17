10bdffa9fc53987a5e5a2de328ac39125c929644
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-17 10:02:19 +0100

    sum which is legal and higher than the minimum found

--- day_17/Program.fs
+++ day_17/Program.fs
@@ -1,12 +1,36 @@
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.LinearAlgebra.Double
+
+
 module Input =
     open System
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list list = 
+    type Map = int [] [] 
+    let readInit (filePath: string): Map =  
         File.ReadAllLines filePath
-            |> Array.map (fun l -> l.ToCharArray() |> Array.map (fun c -> int (c.ToString())) |> Array.toList) 
-            |> Array.toList
+            |> Array.map (fun l -> l.ToCharArray() |> Array.map (fun c -> int (c.ToString())))
+
+    type Dir = Left | Straight | Right
+
+    // low startofcost initially to avoid deep searches
+    let initialMinCost (map: Map): int =
+        let matrix = map |> Array.map (fun r -> r |> Array.map float)
+                         |> DenseMatrix.OfRowArrays 
+        let diag = matrix.Diagonal().MapIndexed(fun i n -> if i = 0 then 0.0 else n)
+        let diag2 = matrix.RemoveColumn(0).Diagonal()
+        let diag3 = matrix.RemoveRow(0).Diagonal()
+        let sum1 = diag.Sum() + diag2.Sum()
+        let sum2 = diag.Sum() + diag3.Sum()
+        let minsum = min sum1 sum2
+        int minsum 
+
+    [<Fact>]
+    let sumsTest () = 
+        let input = readInit "testinput.txt" 
+        Assert.Equal(133, initialMinCost input)
+
 
     [<Fact>]
     let test2 () = 
