f170cc2cb5a5d7abfeb801fa2de047dc00127744
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-17 09:29:26 +0100

    Update Program.fs

--- day_17/Program.fs
+++ day_17/Program.fs
@@ -5,12 +5,16 @@ module Input =
 
     let readInit (filePath: string): int list list = 
         File.ReadAllLines filePath
-            |> Array.map (fun l -> l.ToCharArray() |> Array.map int |> Array.toList) 
+            |> Array.map (fun l -> l.ToCharArray() |> Array.map (fun c -> int (c.ToString())) |> Array.toList) 
             |> Array.toList
 
     [<Fact>]
     let test2 () = 
         let input = readInit "testinput.txt" 
-        Assert.Equal(10, input.Length) 
+        Assert.Equal(13, input.Length) 
+        Assert.Equal(13, input[0].Length) 
+        Assert.Equal(2, input[0][0]) 
+        Assert.Equal(3, input[12][12]) 
+        Assert.Equal(4, input[0][1]) 
 
 module Program = let [<EntryPoint>] main _ = 0
