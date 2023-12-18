8655de4369168cc987f2dfd1b157818ce2e16628
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-18 06:30:37 +0100

    Update Program.fs

--- day_18/Program.fs
+++ day_18/Program.fs
@@ -6,7 +6,9 @@ module Input =
     type Dir = U | D | L | R
     type RGB = string
     type Command = Dir * int * RGB 
-
+    type Position = int*int
+    type Wall = Position*RGB
+    type OutLine = Map<Position,RGB>
 
     let parseCommand (cmdString: string): Command = 
         let parts = cmdString.Split(" ")
@@ -23,7 +25,23 @@ module Input =
         File.ReadAllLines filePath
             |> Array.toList
             |> List.map parseCommand
+
+    let digOutline (commands: Command list): OutLine =
+        let rec digger (commands: Command list) (current: Position) : Wall seq =
+            seq {
+                match commands with
+                    | [] -> () 
+                    | (a,b,c)::t -> yield current,c
+                                    yield! digger t current 
+            }
+        let wall = digger commands (0,0)
+        Map.ofSeq wall 
  
+    [<Fact>]
+    let testOutline () =
+        Assert.Equal<OutLine>(Map.empty, digOutline [])
+        Assert.Equal<OutLine>(Map.ofList[(0,0),"a";(1,0),"a";(2,0),"a"], digOutline [R 3 "a"])
+
     [<Fact>]
     let parseCommandTest () =
         Assert.Equal<Command>((R, 6,"(#70c710)"), parseCommand "R 6 (#70c710)")
