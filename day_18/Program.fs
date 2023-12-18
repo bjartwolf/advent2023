8410417472acfad9bab7d825f218c3e87d2f2c0d
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-18 06:21:06 +0100

    parse ok

--- day_18/Program.fs
+++ day_18/Program.fs
@@ -3,15 +3,34 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    type Dir = U | D | L | R
+    type RGB = string
+    type Command = Dir * int * RGB 
+
+
+    let parseCommand (cmdString: string): Command = 
+        let parts = cmdString.Split(" ")
+        let cmd = match parts[0] with 
+                        |"L" -> L
+                        |"R"-> R
+                        |"U"-> U
+                        |"D"-> D
+                        | x -> failwithf "ouch %A %A" x parts[0]
+        let steps = int parts[1]
+        cmd,steps,parts[2]
+
+    let readInit (filePath: string): Command list = 
+        File.ReadAllLines filePath
+            |> Array.toList
+            |> List.map parseCommand
+ 
+    [<Fact>]
+    let parseCommandTest () =
+        Assert.Equal<Command>((R, 6,"(#70c710)"), parseCommand "R 6 (#70c710)")
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let input = readInit "testinput.txt" 
+        Assert.Equal(14, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
