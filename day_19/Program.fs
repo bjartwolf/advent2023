b9959df193bdf5ce2926e33d4e9ed9cb7342c623
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-19 06:19:28 +0100

    Update Program.fs

--- day_19/Program.fs
+++ day_19/Program.fs
@@ -6,6 +6,15 @@ module Input =
     let readInit (filePath: string): int list = 
         [1]
 
+    type Cat = X | M | A | S 
+    type Operation = Gt | Lt
+    type Rule = Cat * Operation * int
+    type NodeTerminator = NextNode of string | Accepted | Rejected
+    type Node = Rule list * NodeTerminator 
+
+    let tfN: Node =  [ (A, Lt, 2006); M, Gt, 2090], NextNode "rfg"
+
+
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
