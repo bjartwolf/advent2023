1a324a63013fbccc4203692de9bc6cb9813ef19e
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-19 06:27:53 +0100

    Update Program.fs

--- day_19/Program.fs
+++ day_19/Program.fs
@@ -3,16 +3,26 @@ module Input =
     open System.IO
     open Xunit 
 
+    type Cat = X | M | A | S 
+    type Operation = Gt | Lt // use A > 0 
+    type NodeTerminator = N of string | Acc| Rej
+    type Rule = Cat * Operation * int * NodeTerminator
+    type Node = Rule list 
+    type Map = Map<string, Node>
+
+    let tn1: Node =  [ A, Lt, 2006, N "qkq"; M, Gt, 2090, Acc; A, Gt, 0, N "rfg"]
+
+    let parseLine (line: string): (string*Node) = 
+        "px", tn1
+    
+    [<Fact>]
+    let testParseLine() = 
+        Assert.Equal(("px", tn1), parseLine "px{a<2006:qkq,m>2090:A,rfg}") 
+
+
     let readInit (filePath: string): int list = 
         [1]
 
-    type Cat = X | M | A | S 
-    type Operation = Gt | Lt
-    type Rule = Cat * Operation * int
-    type NodeTerminator = NextNode of string | Accepted | Rejected
-    type Node = Rule list * NodeTerminator 
-
-    let tfN: Node =  [ (A, Lt, 2006); M, Gt, 2090], NextNode "rfg"
 
 
     [<Fact>]
