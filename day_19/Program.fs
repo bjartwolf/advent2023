89cf1b72e15482c0db6d2e1ebadb40503921f4e7
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2023-12-19 06:39:00 +0100

    oki

--- day_19/Program.fs
+++ day_19/Program.fs
@@ -13,10 +13,33 @@ module Input =
     let tn1: Node =  [ A, Lt, 2006, N "qkq"; M, Gt, 2090, Acc; A, Gt, 0, N "rfg"]
 
     let parseLine (line: string): (string*Node) = 
-        "px", tn1
+        let foo = line.Split("{") 
+        let id = foo[0]
+        let rawRules = foo[1].Replace("}","").Split(",")
+        let rules = [
+            for rawRule in rawRules do
+                let gt = rawRule.Contains(">")
+                if gt then
+                    let parts = rawRule.Split(">")
+                    if (parts[1].Contains(":")) then
+                        let bar = match parts[0] with 
+                                                    | "x" -> X
+                                                    | "m" -> M 
+                                                    | "a" -> A
+                                                    | "s" -> S
+                        let baz = parts[1].Split(":")
+                        let num = int baz[0] 
+                        let stop = match baz[1] with
+                            | "A" -> Acc
+                            | "R" -> Rej
+                            | str -> N str
+                        yield bar,Gt,num,stop
+        ]
+        id, rules 
     
     [<Fact>]
     let testParseLine() = 
+        Assert.Equal(("ax", [A,Gt,2006, N "qkq"] ), parseLine "ax{a>2006:qkq}") 
         Assert.Equal(("px", tn1), parseLine "px{a<2006:qkq,m>2090:A,rfg}") 
 
 
